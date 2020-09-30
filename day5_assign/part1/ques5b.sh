a5eea999f06daa90d3bdea12c5797bccd568b001
ashu3902 <moreashutosh1997@gmail.com>
2020-09-30 19:37:53 +0530

    3rd modification

--- day5_assign/part1/ques5b.sh
+++ day5_assign/part1/ques5b.sh
@@ -2,8 +2,13 @@
 
 echo "Enter length and breadth of field: " a b
 read a b
+c=25;
 area=$(( $a*$b ))
 echo "$area"
+sqmtr=$(awk 'BEGIN{print "Area in sq meters is: " '$area' / 10.764}')
 acre=$(awk 'BEGIN{print "Area in acres is: " '$area' / 43560}')
+
+mul=$(( $area*$c ))
+echo "Area of 25 plots is: $mul sq mtrs"
 
 
