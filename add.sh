439552dcfcd96ba709b0101b50787d27d1aef34b
ashu3902 <moreashutosh1997@gmail.com>
2020-10-03 19:44:29 +0530

    conflict demo

--- add.sh
+++ add.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-read -p "Enter first number: " x
-read -p "Enter second number: " y
-z=$(( $x + $y ))
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+z=$(( $a + $b ))
 echo $z
